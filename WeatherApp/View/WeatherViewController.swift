fe79b909ff95abca5ca2724f5987792bd9aa15a5
Nicolas Pepe <Nicolasnahuel94@gmail.com>
2023-01-31 08:10:14 -0300

    Add activityIndicator with combine

--- WeatherApp/View/WeatherViewController.swift
+++ WeatherApp/View/WeatherViewController.swift
@@ -13,13 +13,17 @@ class WeatherViewController: UIViewController {
     var viewModel = WeatherViewModel()
     var wm: WeatherModel?
     var anyCancellable: [AnyCancellable] = []
+    var locations: [(String,String)] = [("-34.61315","-58.37723"),("-38.00042","-57.5562")]
+    
+    
     @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var labelTemp: UILabel!
     @IBOutlet weak var backgroundImage: UIImageView!
-
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityIndicator.startAnimating()
         subscriptions()
         viewModel.getWeather(latitude: "-34.61315", longitude: "-58.37723")
         backgroundImage.image = UIImage(named: "defaultSunset")
@@ -27,17 +31,26 @@ class WeatherViewController: UIViewController {
     }
     
     func subscriptions(){
+        activityIndicator.hidesWhenStopped = true
         viewModel.$weatherList.sink {_ in } receiveValue: { wm in
             self.wm = wm
             self.myLabel.text = wm?.name
             self.labelTemp.text = wm?.main?.temp.description
         }.store(in: &anyCancellable)
+        
+        viewModel.$loading.sink { state in
+            guard let state = state else {return}
+            self.configLoading(state: state)
+        }.store(in: &anyCancellable)
     }
  
+    private func configLoading(state: Bool) {
+        if state {
+            activityIndicator.startAnimating()
+        } else {activityIndicator.stopAnimating()}
+    }
     
     @IBAction func SwipeAction(_ sender: Any) {
         viewModel.getWeather(latitude: "-38.00042", longitude: "-57.5562")
     }
-    
-    
 }
--- WeatherApp/ViewModel/WeatherViewModel.swift
+++ WeatherApp/ViewModel/WeatherViewModel.swift
@@ -11,14 +11,16 @@ import Alamofire
 
 class WeatherViewModel {
     @Published var weatherList: WeatherModel?
+    @Published var loading: Bool?
     //Creo subject para que se suscriban
     
     var service = WeatherService.shared
     func getWeather(latitude: String, longitude: String){
         
-    
+        self.loading = true
         WeatherService.shared.getWeather(latitude: latitude, longitude: longitude) { weather in
             self.weatherList = weather
+            self.loading = false
         } failure: { error in
             print(error)
         }
